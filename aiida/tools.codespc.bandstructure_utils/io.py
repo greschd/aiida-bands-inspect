9f071889255e5ab065fe41e3fb77b543466454c1
Dominik Gresch <greschd@gmx.ch>
2017-04-06 17:53:09 +0200

    Handle different 'kinds' of KpointData in write_kpoints function

--- aiida/tools.codespc.bandstructure_utils/io.py
+++ aiida/tools.codespc.bandstructure_utils/io.py
@@ -9,6 +9,16 @@ import h5py
 import numpy as np
 
 def write_kpoints(kpoints_data, filename):
+    # This can be replaced with bandstructure_utils.io functions when
+    # AiiDA supports Python 3.
     with h5py.File(filename, 'w') as f:
-        f['type_tag'] = 'kpoints_explicit'
-        f['kpoints'] = np.array(kpoints_data.get_kpoints())
+        attrs = kpoints_data.attrs()
+        if 'mesh' in attrs:
+            f['type_tag'] = 'kpoints_mesh'
+            f['mesh'] = np.array(attrs['mesh'])
+            f['offset'] = np.array(attrs['offset'])
+        elif 'array|kpoints' in attrs
+            f['type_tag'] = 'kpoints_explicit'
+            f['kpoints'] = np.array(kpoints_data.get_kpoints())
+        else:
+            raise NotImplementedError("Unrecognized KpointsData form, has attrs '{}'".format(attrs))
