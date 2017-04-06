59956452ebd6101c04d403dddb7afaa6c5571d4b
Dominik Gresch <greschd@gmx.ch>
2017-04-06 17:01:59 +0200

    Add missing numpy import

--- aiida/tools.codespc.bandstructure_utils/io.py
+++ aiida/tools.codespc.bandstructure_utils/io.py
@@ -6,6 +6,7 @@
 from __future__ import division, print_function, unicode_literals
 
 import h5py
+import numpy as np
 
 def write_kpoints(kpoints_data, filename):
     with h5py.File(filename, 'w') as f:
