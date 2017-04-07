529beca127b4612b02adce83e9cb57dad902545d
Dominik Gresch <greschd@gmx.ch>
2017-04-07 15:06:03 +0200

    Use cls in super() call

--- aiida/orm.calc.job.bandstructure_utils/difference.py
+++ aiida/orm.calc.job.bandstructure_utils/difference.py
@@ -11,7 +11,7 @@ from aiida.common.datastructures import CalcInfo, CodeInfo
 class DifferenceCalculation(JobCalculation):
     @classproperty
     def _use_methods(cls):
-        retdict = super(DifferenceCalculation, self)._use_methods
+        retdict = super(cls, cls)._use_methods
         retdict['bands'] = dict(
             valid_types=DataFactory('array.bands'),
             additional_parameter=1,
