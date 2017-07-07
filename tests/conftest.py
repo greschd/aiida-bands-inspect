1aa474ffbd78b37c0d5ba245ada694923a6a251a
Dominik Gresch <greschd@gmx.ch>
2017-07-07 18:04:19 +0200

    Update conftest to new aiida-pytest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,4 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os
-import pytest
-
-from aiida_pytest.configure import *
-from aiida_pytest.process import *
-
-@pytest.fixture
-def sample():
-    def inner(name):
-        return os.path.join(os.path.abspath('./samples'), name)
-    return inner
+from aiida_pytest import * 
