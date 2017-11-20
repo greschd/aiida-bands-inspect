5e41aa86937a25dc1794397a9d5d0147fdec3d7b
Dominik Gresch <greschd@gmx.ch>
2017-11-20 17:41:39 +0100

    Change config to include full bands-inspect path.

--- .travis-data/configure.py
+++ .travis-data/configure.py
@@ -4,15 +4,18 @@ Usage: python configure.py travis_data_folder test_folder
 """
 
 import sys
+import subprocess
 from os.path import join
 
 travis_data_folder = sys.argv[1]
 in_file = join(travis_data_folder, 'test_config.yml')
-# wannier90_path = join(travis_data_folder, 'wannier90/wannier90.x')
+bands_inspect_path = subprocess.check_output(
+    'which bands-inspect', shell=True
+).decode().strip()
 
 out_file = join(sys.argv[2], 'config.yml')
 
 with open(in_file, 'r') as f:
-    res = f.read().format()
+    res = f.read().format(bands_inspect_path=bands_inspect_path)
 with open(out_file, 'w') as f:
     f.write(res)
--- .travis-data/test_script.sh
+++ .travis-data/test_script.sh
@@ -6,8 +6,7 @@ set -ev
 case "$TEST_TYPE" in
     tests)
         # Run the AiiDA tests
-        cp ${TRAVIS_BUILD_DIR}/.travis-data/test_config.yml ${TRAVIS_BUILD_DIR}/tests/config.yml;
-        # python ${TRAVIS_BUILD_DIR}/.travis-data/configure.py ${TRAVIS_BUILD_DIR}/.travis-data ${TRAVIS_BUILD_DIR}/tests;
+        python ${TRAVIS_BUILD_DIR}/.travis-data/configure.py ${TRAVIS_BUILD_DIR}/.travis-data ${TRAVIS_BUILD_DIR}/tests;
         export AIIDA_PATH="${TRAVIS_BUILD_DIR}/tests"
         cd ${TRAVIS_BUILD_DIR}/tests; py.test --quiet-wipe
         ;;
