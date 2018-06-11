3f08c1473bd4663db12a4200ccb9723f08cfa329
Dominik Gresch <greschd@gmx.ch>
2018-06-11 17:19:08 +0200

    Remove legacy tests.

--- tests/test_difference.py
+++ tests/test_difference.py
@@ -34,28 +34,6 @@ def get_bands_builder(get_process_builder):
     return builder
 
 
-@pytest.fixture
-def get_legacy_calc(get_bands_builder):
-    def inner():
-        from aiida.orm import Code
-        from aiida_bands_inspect.calculations.difference import DifferenceCalculation
-        calc = DifferenceCalculation()
-        builder = get_bands_builder
-        inputs = builder._todict()
-        for key, value in inputs.items():
-            if key.startswith('_') or key in ['dynamic', 'options']:
-                continue
-            getattr(calc, 'use_{}'.format(key))(value)
-        code = Code.get_from_string('bands_inspect')
-        calc.use_code(code)
-        calc.set_computer(code.get_computer().name)
-        calc.set_withmpi(False)
-        calc.set_resources({'num_machines': 1})
-        return calc
-
-    return inner
-
-
 def test_difference(configure_with_daemon, get_bands_builder):
     from aiida.work.launch import run_get_node
     from aiida.orm import load_node
@@ -71,18 +49,6 @@ def test_difference(configure_with_daemon, get_bands_builder):
         )
     )
     assert np.isclose(output['difference'].value, 1 / 3)
-
-
-def test_difference_legacy(configure_with_daemon, get_legacy_calc):
-    """
-    Test DifferenceCalculation through the legacy calculation interface.
-    """
-    calc = get_legacy_calc()
-    calc.store_all()
-    calc.submit()
-    while not calc.is_finished:
-        time.sleep(1)
-    assert np.isclose(calc.out.difference.value, 1 / 3)
 
 
 @skip_caching
@@ -102,21 +68,3 @@ def test_difference_cache(
     c1.get_hash(ignore_errors=False)
     assert '_aiida_cached_from' in c2.extras()
     assert output1['difference'] == output2['difference']
-
-
-@skip_caching
-def test_difference_legacy_cache(configure_with_daemon, get_legacy_calc):
-    """
-    Test DifferenceCalculation caching through the legacy calculation interface.
-    """
-    calc1 = get_legacy_calc()
-    calc2 = get_legacy_calc()
-    calc1.store_all(use_cache=True)
-    if not calc1.is_finished:
-        calc1.submit()
-    while not calc1.is_finished:
-        time.sleep(1)
-    calc2.store_all(use_cache=True)
-    assert calc2.is_finished
-    assert np.isclose(calc1.out.difference.value, calc2.out.difference.value)
-    assert '_aiida_cached_from' in calc2.extras()
