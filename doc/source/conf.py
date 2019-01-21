6eaa8790aa0efc5cd94e4f879831123b1d5a58a6
Dominik Gresch <greschd@gmx.ch>
2019-01-21 12:07:45 +0100

    Add specific instructions for RTD compilation.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -3,8 +3,32 @@
 # © 2017-2019, ETH Zurich, Institut für Theoretische Physik
 # Author: Dominik Gresch <greschd@gmx.ch>
 
+import sys, os
+
+sys.path.append(os.path.join(os.path.dirname(__file__), os.pardir))
+
 import aiida
-aiida.try_load_dbenv()
+from aiida.backends import settings
+
+# We set that we are in documentation mode - even for local compilation
+settings.IN_DOC_MODE = True
+
+# on_rtd is whether we are on readthedocs.org, this line of code grabbed
+# from docs.readthedocs.org
+# NOTE: it is needed to have these lines before load_dbenv()
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    html_theme = 'sphinx_rtd_theme'
+    # Loading the dbenv. The backend should be fixed before compiling the
+    # documentation.
+    aiida.try_load_dbenv()
+else:
+    # Back-end settings for readthedocs online documentation.
+    # from aiida.backends import settings
+    settings.BACKEND = "django"
+    settings.AIIDADB_PROFILE = "default"
+
 import aiida_bands_inspect
 
 # -- General configuration ------------------------------------------------
@@ -66,11 +90,6 @@ pygments_style = 'sphinx'
 todo_include_todos = False
 
 # -- Options for HTML output ----------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
