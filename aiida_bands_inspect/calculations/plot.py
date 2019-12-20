5f52861e3f5e0dfe8cd71513d9e2db8cf134bd32
Dominik Gresch <greschd@gmx.ch>
2019-12-20 13:35:44 +0100

    Replace uses of deprecated 'write_bands'.

--- aiida_bands_inspect/calculations/plot.py
+++ aiida_bands_inspect/calculations/plot.py
@@ -10,7 +10,7 @@ from aiida import orm
 from aiida.engine import CalcJob
 from aiida.common import CalcInfo, CodeInfo
 
-from ..io import write_bands
+from .. import io
 
 __all__ = ('PlotCalculation', )
 
@@ -71,9 +71,9 @@ class PlotCalculation(CalcJob):
         ev1_filename = 'eigenvals1.hdf5'
         ev2_filename = 'eigenvals2.hdf5'
         eigenval_file_1 = tempfolder.get_abs_path(ev1_filename)
-        write_bands(self.inputs.bands1, eigenval_file_1)
+        io.write(self.inputs.bands1, eigenval_file_1)
         eigenval_file_2 = tempfolder.get_abs_path(ev2_filename)
-        write_bands(self.inputs.bands2, eigenval_file_2)
+        io.write(self.inputs.bands2, eigenval_file_2)
 
         calcinfo = CalcInfo()
         calcinfo.uuid = self.uuid
