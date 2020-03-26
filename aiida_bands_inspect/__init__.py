1a74d9a1cb2657cd6c1a3be5203cb690ecce90c5
Dominik Gresch <greschd@gmx.ch>
2020-03-26 10:51:10 +0100

    Read bands file from 'result_filename' option
    Before, it would be read from the 'output_filename' option. This
    is still retained as a fallback, to be compatible with older
    versions of aiida-tbmodels which don't have the result_filename
    option.

--- aiida_bands_inspect/__init__.py
+++ aiida_bands_inspect/__init__.py
@@ -3,7 +3,7 @@
 # © 2017-2019, ETH Zurich, Institut für Theoretische Physik
 # Author: Dominik Gresch <greschd@gmx.ch>
 
-__version__ = "0.3.1"
+__version__ = "0.4.0"
 
 from . import calculations
 from . import parsers
